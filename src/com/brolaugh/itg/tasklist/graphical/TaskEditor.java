9c79f9a8b58eb365cc2657902153546945325ce6
Hannes Kindströmmer <me@brolaugh.com>
2016-05-20 10:59:30 +0200

    Forgott to stage this file in last commit

---
+++ src/com/brolaugh/itg/tasklist/graphical/TaskEditor.java
@@ -0,0 +1,15 @@
+package com.brolaugh.itg.tasklist.graphical;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TaskEditor extends Application{
+    public TaskEditor() {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
